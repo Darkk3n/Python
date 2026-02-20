dd58a17c66eaae64c359e470cce9436418c434f1
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-02-19 19:11:38 -0600

    fixed folder name

---
+++ challenges/while_countdown.py
@@ -0,0 +1,12 @@
+from os import system
+
+system("clear")
+
+# The "While" Countdown: Count down from 10 to 1 with a while loop, ending with "Blast off!".
+
+counter = 10
+while counter <= 10 and counter >= 1:
+    print(counter)
+    counter -= 1
+
+print("Blass Off!")
